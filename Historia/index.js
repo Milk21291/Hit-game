1dab8b9725024a0fe96cfa7e624a5c1430daf577
João Victor <90943414+Milk21291@users.noreply.github.com>
2022-10-19 17:11:02 -0300

    Add files via upload

---
+++ Historia/index.js
@@ -0,0 +1,3 @@
+let name = prompt('Digite seu nome')
+let history = document.getElementById('history')
+history.innerHTML = `Olá ${name}, irei te contar a historia que aconteceu recentemente no mundo Hitty.`
