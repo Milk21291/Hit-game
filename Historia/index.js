b9d922cf59d150b59cd217ae1b9ee499a7523ef9
João Victor <milk21291br@gmail.com>
2022-10-20 13:29:09 -0300

    teste

--- Historia/index.js
+++ Historia/index.js
@@ -1,3 +1,3 @@
 let name = prompt('Digite seu nome')
 let history = document.getElementById('history')
-history.innerHTML = `Olá ${name}, irei te contar a historia que aconteceu recentemente no mundo Hitty.`
+history.innerHTML = `Olá ${name}, irei te contar a historia que aconteceu recentemente no mundo Hitty.<br />`
