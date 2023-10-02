e7b71292b193d5e9240eb6fc5363f8e2d05a39ad
João Victor <milk21291br@gmail.com>
2023-10-02 15:58:34 -0300

    historia feita e jogo modificado

--- Historia/index.js
+++
@@ -1,3 +0,0 @@
-let name = prompt('Digite seu nome')
-let history = document.getElementById('history')
-history.innerHTML = `Olá ${name}, irei te contar a historia que aconteceu recentemente no mundo Hitty.<br />`
--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-let numeroSecreto = parseInt(Math.random() * 20) + 1;
-let numeroTentativas = 4;
+let numeroSecreto = gerarNumeroSecreto();
+let numeroTentativas = 20;
+let faseAtual = 1;
+console.log(numeroSecreto)
+
+function gerarNumeroSecreto() {
+    return parseInt(Math.random() * 20) + 1;
+}
 
 function Chutar() {
   let resultado = document.getElementById("resultado");
   let tentativas = document.getElementById("tentativas");
   let chute = parseInt(document.getElementById("valor").value);
+  let fases = document.getElementById('fases');
   
+  fases.innerHTML = `Fase ${faseAtual}`;
+
+  if (!chute) {
+    resultado.innerHTML = "Valor inválido";
+    return;
+  }
+
   if (numeroTentativas > 0) {
     if (chute == numeroSecreto) {
-      resultado.innerHTML = "Parabéns!! Você acertou.";
-      numeroTentativas === 1;
-      tentativas.innerHTML = "Recarregue a página para jogar novamente.";
+      resultado.innerHTML = `Parabéns!! Você acertou. Você está na Fase ${faseAtual + 1}, escolha um número.`;
+      tentativas.innerHTML = "Avançando para a próxima fase...";
+      faseAtual++;
+      numeroTentativas - 2
+      numeroSecreto = gerarNumeroSecreto();
     } else if (chute > 20 || chute < 1) {
       resultado.innerHTML = "Número inválido. Digite um número entre 1 e 20.";
     } else {
       numeroTentativas--;
-      tentativas.innerHTML =
-      "Você tem " + numeroTentativas + " tentativas restante.";
+      tentativas.innerHTML = "Você tem " + numeroTentativas + " tentativas restantes.";
       if (numeroSecreto < chute) {
-        resultado.innerHTML =
-        "Ops! Você errou. O número secreto é menor do que " + chute;
+        resultado.innerHTML = "Ops! Você errou. O número secreto é menor do que " + chute;
       } else if (numeroSecreto > chute) {
-        resultado.innerHTML =
-        "Parece que você errou. O número secreto é maior do que " + chute;
+        resultado.innerHTML = "Parece que você errou. O número secreto é maior do que " + chute;
       }
     }
   }
+
   if (numeroTentativas === 0) {
     resultado.innerHTML = "Você perdeu. O número secreto era " + numeroSecreto;
-    tentativas.innerHTML =
-    "Você esgotou suas tentativas. Recarregue a página para jogar novamente.";
-  } 
+    tentativas.innerHTML = "Você esgotou suas tentativas. Recarregue a página para começar novamente.";
+    faseAtual = 1;
+    numeroSecreto = gerarNumeroSecreto();
+  }
 }
-
-
-
-/*
-let delet = document.getElementById("delete");
-let aviso = document.getElementById("aviso");
-if(chute == chute) { 
-aviso.innerHTML = `Você não pode usar novamente o número ${chute}.`
-  }  
-*/
-// if (chute > 15 || chute < 0) {
-//   elementoResultado.innerHTML = "Você deve digitar um numero de 0 a 15";
-
-// else if (chute > 15 || chute < 0) {
-// elementoResultado.innerHTML = "Você deve digitar um numero de 0 a 15";
-// } else {
-// console.log("errou")
-// }
