f675a3a1323d08295b615bbed5217616b4f9f675
João Victor <milk21291br@gmail.com>
2023-10-03 11:33:04 -0300

    tentativas atualizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,68 @@
-(function() {
+(function () {
   let numeroSecreto = gerarNumeroSecreto();
-  let numeroTentativas = 20;
+  const tentativasBase = 20;
+  let numeroTentativas = tentativasBase;
   let faseAtual = 1;
+  let tentativaAtual = 0;
 
   function gerarNumeroSecreto() {
-      return parseInt(Math.random() * 20) + 1;
+    return parseInt(Math.random() * 20) + 1;
   }
 
-  window.Chutar = function() {
-      let resultado = document.getElementById("resultado");
-      let tentativas = document.getElementById("tentativas");
-      let chute = parseInt(document.getElementById("valor").value);
-      let fases = document.getElementById('fases');
-      
-      fases.innerHTML = `Fase ${faseAtual}`;
+  function calcularTentativasIniciais() {
+    tentativaAtual += 2;
+    return tentativasBase - tentativaAtual;
+  }
 
-      if (!chute) {
-          resultado.innerHTML = "Valor inválido";
+  window.Chutar = function () {
+    let resultado = document.getElementById("resultado");
+    let tentativas = document.getElementById("tentativas");
+    let chute = parseInt(document.getElementById("valor").value);
+    let fases = document.getElementById("fases");
+
+    fases.innerHTML = `Fase ${faseAtual}`;
+
+    if (!chute) {
+      resultado.innerHTML = "Valor inválido";
+      return;
+    }
+
+    if (numeroTentativas > 0) {
+      if (chute == numeroSecreto) {
+        if (faseAtual === 7) {
+          resultado.innerHTML = `Parabéns!! Você completou o game, agora vá em "Historia" e veja como ela termina.`;
           return;
-      }
-  
-      if (numeroTentativas > 0) {
-          if (chute == numeroSecreto) {
-              if (faseAtual === 7) {
-                  resultado.innerHTML = `Parabéns!! Você completou o game, agora vá em "Historia" e veja como ela termina.`;
-                  return;
-              }
-              resultado.innerHTML = `Parabéns!! Você acertou. Você está na Fase ${faseAtual + 1}, escolha um número.`;
-              tentativas.innerHTML = "Avançando para a próxima fase...";
-              faseAtual++;
-              numeroTentativas -= 2;
-              numeroSecreto = gerarNumeroSecreto();
-          } else if (chute > 20 || chute < 1) {
-              resultado.innerHTML = "Número inválido. Digite um número entre 1 e 20.";
-          } else {
-              numeroTentativas--;
-              tentativas.innerHTML = "Você tem " + numeroTentativas + " tentativas restantes.";
-              if (numeroSecreto < chute) {
-                  resultado.innerHTML = "Ops! Você errou. O número secreto é menor do que " + chute;
-              } else if (numeroSecreto > chute) {
-                  resultado.innerHTML = "Parece que você errou. O número secreto é maior do que " + chute;
-              }
-          }
-      }
-  
-      if (numeroTentativas === 0) {
-          resultado.innerHTML = "Você perdeu. O número secreto era " + numeroSecreto;
-          tentativas.innerHTML = "Você esgotou suas tentativas. Recarregue a página para começar novamente.";
-          faseAtual = 1;
-          numeroSecreto = gerarNumeroSecreto();
-      }
-  }
-})();
+        }
 
+        faseAtual++;
+        numeroTentativas = calcularTentativasIniciais();
+        resultado.innerHTML = `Parabéns!! Você acertou. Você está na Fase ${faseAtual}, escolha um número.`;
+        numeroSecreto = gerarNumeroSecreto();
+      } else if (chute > 20 || chute < 1) {
+        resultado.innerHTML = "Número inválido. Digite um número entre 1 e 20.";
+      } else {
+        numeroTentativas--;
+        tentativas.innerHTML =
+          "Você tem " + numeroTentativas + " tentativas restantes.";
+        if (numeroSecreto < chute) {
+          resultado.innerHTML =
+            "Ops! Você errou. O número secreto é menor do que " + chute;
+        } else if (numeroSecreto > chute) {
+          resultado.innerHTML =
+            "Parece que você errou. O número secreto é maior do que " + chute;
+        }
+      }
+    }
+
+    if (numeroTentativas === 0) {
+      resultado.innerHTML =
+        "Você perdeu. O número secreto era " + numeroSecreto;
+      tentativas.innerHTML =
+        "Você esgotou suas tentativas. Recarregue a página para começar novamente.";
+      faseAtual = 1;
+      tentativaAtual = 0;
+      numeroTentativas = tentativasBase;
+      numeroSecreto = gerarNumeroSecreto();
+    }
+  };
+})();
